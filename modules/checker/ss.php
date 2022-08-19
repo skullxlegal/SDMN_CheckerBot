bf9de16f0ae9d4b7e4b44e210dad05103959aed7
skullxlegal <111497111+skullxlegal@users.noreply.github.com>
2022-08-19 18:40:54 +0530

    Update ss.php

--- modules/checker/ss.php
+++ modules/checker/ss.php
@@ -20,24 +20,6 @@ include_once __DIR__."/../functions/functions.php";
 if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){   
     $antispam = antispamCheck($userId);
     addUser($userId);
-    
-    if($antispam != False){
-      bot('sendmessage',[
-        'chat_id'=>$chat_id,
-        'text'=>"[<u>ANTI SPAM</u>] Try again after <b>$antispam</b>s.",
-        'parse_mode'=>'html',
-        'reply_to_message_id'=> $message_id
-      ]);
-      return;
-
-    }else{
-        $messageidtoedit1 = bot('sendmessage',[
-          'chat_id'=>$chat_id,
-          'text'=>"<b>Wait for Result...</b>",
-          'parse_mode'=>'html',
-          'reply_to_message_id'=> $message_id
-
-        ]);
 
         $messageidtoedit = capture(json_encode($messageidtoedit1), '"message_id":', ',');
         $lista = substr($message, 4);
@@ -286,7 +268,7 @@ Time -» <b>$time</b><b>s</b>
               'disable_web_page_preview'=>'true'
               
           ]);
-      }
+      
     }
 }
 
